3f51b5960364487415e7c4cb28d3f2146a7f3c1d
Jobina Arinze <113374279+Jobijollof@users.noreply.github.com>
2023-03-25 06:07:39 +0100

    Update app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -195,10 +195,13 @@ def data_activities():
     return model['data'], 200
   return
 
+
 @app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
+#@xray_recorder.capture('activities_show')
 def data_show_activity(activity_uuid):
   data = ShowActivity.run(activity_uuid=activity_uuid)
   return data, 200
+
 
 @app.route("/api/activities/<string:activity_uuid>/reply", methods=['POST','OPTIONS'])
 @cross_origin()
