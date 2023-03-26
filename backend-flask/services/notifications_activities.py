a42a3b2dcd562e3f2337d93e982f21741ba3e8b6
Jobina Arinze <113374279+Jobijollof@users.noreply.github.com>
2023-03-26 22:45:19 +0100

    fixed a bug

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -2,11 +2,10 @@ from datetime import datetime, timedelta, timezone
 class NotificationsActivities:
   def run():
     now = datetime.now(timezone.utc).astimezone()
-    
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Jobi Jollof',
-      'message': 'Elections in Nigeria!',
+      'handle':  'coco',
+      'message': 'I am white unicorn',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
